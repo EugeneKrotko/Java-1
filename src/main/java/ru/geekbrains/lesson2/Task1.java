1386dd292bca5dac2b133483e793ac0a6f336eba
EugeneKrotko <evko777@gmail.com>
2020-12-29 14:55:59 +0300

    ПЗ номер 2

--- src/main/java/ru/geekbrains/lesson2/Task1.java
+++ src/main/java/ru/geekbrains/lesson2/Task1.java
@@ -2,6 +2,19 @@ package ru.geekbrains.lesson2;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        changeArray();
+    }
+    static void changeArray() {
+        int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.print("BEFORE: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        System.out.print("\nAFTER:  ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
